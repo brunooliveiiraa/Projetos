9e30e2aeb24359d163b2192b61cb1cafd0719461
brunooliveiira <97993312+brunooliveiira@users.noreply.github.com>
2022-08-01 16:34:44 -0300

    commit api

---
+++ tutorial-rest-api/src/test/java/expertostech/tutorialrestapi/TutorialRestApiApplicationTests.java
@@ -0,0 +1,14 @@
+package expertostech.tutorialrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TutorialRestApiApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialRestApiApplication.class, args);
+	}
+
+}
